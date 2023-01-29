f4ed51fe4b046b536251f2c0173b61572a81d14a
Dandelionym <dandelionatcha@163.com>
2023-01-29 14:10:57 +0800

    storage random method

---
+++ chain_actiavtion_field.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    INFORMATION TITLE
+    -----------------------------------------------------------------
+    AUTHOR: Mellen Y.Pu
+    DATE: 2023/1/29 下午2:10
+    FILE: chain_actiavtion_field.py
+    PROJ: EvoMorph
+    IDE: PyCharm
+    EMAIL: yingmingpu@gmail.com
+    ----------------------------------------------------------------- 
+                                      ONE DOOR OPENS ALL THE WINDOWS.
+
+    @INTRODUCTION: 
+     - 
+    @FUNCTIONAL EXPLATION:
+     - 
+    @LAUNCH:
+     - 
+"""
